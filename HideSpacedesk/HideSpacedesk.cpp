1ace3e179799d28055fa114587b08c3a0fe73bce
Under4groos <hhit934@gmail.com>
2022-11-20 16:08:52 +0300

    Добавьте файлы проекта.

---
+++ HideSpacedesk/HideSpacedesk.cpp
@@ -0,0 +1,18 @@
+﻿// HideSpacedesk.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+int main()
+{
+    while (true)
+    {
+        HWND hide = FindWindowA("SPACEDESKSERVICETRAYAPPLICATION", NULL);
+        ShowWindow(hide, 0);
+        Sleep(1000);
+    }
+    
+     
+}
+
+ 
